90eb01a388ade234cc6fdd45a582a4fe664b3802
Pedro H Feijó Wasem <115823719+pedroW7439@users.noreply.github.com>
2023-03-06 11:27:48 -0300

    Trabalho com alteração de design e refatoração

---
+++ js/login.js
@@ -0,0 +1,20 @@
+function login(event) {
+    event.preventDefault();
+    const form = document.getElementById("login");
+    const listaArmazenada = JSON.parse(localStorage.getItem("listaDeUsuarios") ?? "[]");
+    if (listaArmazenada === []) {
+        alert("Nenhum usuário cadastrado, por favor, crie uma conta clicando em Criar uma conta");
+        form.reset();
+        return;
+    }
+
+    const usuarioCadastrado = listaArmazenada.find((usuario) => usuario.username === form.username.value && usuario.senha === form.senha.value);
+    if (!usuarioCadastrado) {
+        alert("Usuário ou senha incorretos");
+        form.reset();
+        return;
+    }
+
+    localStorage.setItem("usuarioAtivo", JSON.stringify(usuarioCadastrado));
+    window.location.href = "lembretes.html";
+}
